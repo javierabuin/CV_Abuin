938ebd74a280d07e4cceb4ff2f8d134acc924dc6
Javier Abuin <javierabuin4@gmail.com>
2023-02-17 14:13:22 -0300

    Primer escritura en el repositorio

---
+++ forma.js
@@ -0,0 +1,20 @@
+function ubicacion() {
+	var uno = document.getElementById('ubi');
+  if (uno.textContent == 'Los pinos 2650') 
+  		uno.textContent = 'Ubicacion';
+  else uno.textContent = 'Los pinos 2650'; 
+}
+
+function mail() {
+	var uno = document.getElementById('mail');
+  if (uno.textContent == 'Javierabuin4@gmail.com') 
+  		uno.textContent = 'Mail';
+  else uno.textContent = 'Javierabuin4@gmail.com'; 
+}
+
+function telefono() {
+	var uno = document.getElementById('tel');
+  if (uno.textContent == '3364-340483') 
+  		uno.textContent = 'Telefono';
+  else uno.textContent = '3364-340483'; 
+}
